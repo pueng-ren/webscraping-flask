e5425a48286cbfe823cdd7937612623fb9363743
Saynampung Akkararuengmontri <pung33_@hotmail.co.th>
2021-02-12 11:55:46 +0700

    add file about GCF

--- utils/scraping.py
+++ utils/scraping.py
@@ -4,20 +4,31 @@ import requests
 
 class Webscraping:
 
+    header = [
+            "date"
+            , "open"
+            , "max"
+            , "min"
+            , "avg"
+            , "close"
+            , "change"
+            , "percent_change"
+            , "volume"
+            , "value"
+            , "SETIndex"
+            , "percent_change2"
+        ]
+
     def scraping_page(self, url:str):
         url = requests.get(url)
         data = BeautifulSoup(url.content, "html.parser")
         return data
     
     def get_stock(self, symbol:str):
-        header = []
         stockByDate = {}
         stockByColumn = {}
         scraping_page = self.scraping_page("https://www.settrade.com/C04_02_stock_historical_p1.jsp?txtSymbol="+symbol+"&selectPage=1&max=200")
         tbody = scraping_page.tbody
-
-        for th in scraping_page.thead.findAll('th'): 
-            header.append(th.get_text())
 
         for tr in tbody.findAll('tr'):
             date = tr.findAll('td')[0].get_text()
@@ -26,7 +37,6 @@ class Webscraping:
                 stockByColumn[headerTable] = td.get_text()
             stockByDate[date] = stockByColumn
             stockByColumn = {}
-        
         return stockByDate
 
 
